b8902937c1331f777ae348a81467a70308743ac1
Fabian <ehard.fabian@gmx.net>
2024-08-22 10:51:56 +0200

    day4-finish

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,7 @@
 <x-layout>
+    <x-slot:heading>
+        About Page 
+    </x-slot:heading>
 <h1>Welcome About page</h1>
 
 </x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,8 @@
 <x-layout>
+    <x-slot:heading>
+        Home Page 
+    </x-slot:heading>
+    
 <h1>Welcome homepage😁git </h1>
 
 </x-layout>
