4071418e686898c697fc0c7c924561b10b7548e5
Str1k3r2k <116755366+Str1k3r2k@users.noreply.github.com>
2024-08-27 09:04:31 +0200

    day14 finish pagination

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -5,18 +5,21 @@
     
     <div class="space-y-4">
         @foreach ($jobs as $job)
-            
                 
-                    <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
-                        <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
 
-                        <div>
-                            <b>{{ $job['title'] }}:</b> Pays {{ $job['salary'] }} per year.
-                        </div>
-                    </a>
+                <div>
+                    <b>{{ $job['title'] }}:</b> Pays {{ $job['salary'] }} per year.
+                </div>
+            </a>
                 
-            
         @endforeach
+            
+            <div>
+                {{ $jobs->links() }}
             </div>
+                    
+    </div>
 
 </x-layout>
